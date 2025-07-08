0dd38be295a4d60e72dc7215de1dfea53815fd51
Yonah Aviv <avivyonah@gmail.com>
2025-07-08 12:12:07 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let bar = document.getElementById("barDiv");
+let item = document.getElementById("item");
+bar.addEventListener("click",()=>{
+  if(item.style.right == "-250px"){
+    item.style.right = "0px";
+  }
+  else{
+    item.style.right = "-250px";
+  }
+})
+
